06c621de80aea116826aaccb53091614b1e004cc
unknown <shavotri1@gmail.com>
2022-02-14 13:44:21 -0300

    variaveis-js

---
+++ variaveis/index.js
@@ -0,0 +1,53 @@
+//Analise o programa abaixo e diga o que será impresso no console, SEM EXECUTAR o programa.
+// console.log(b) = 10
+// console.log(a,b) = 10, 5
+
+//Analise o programa abaixo e diga o que será impresso no console, SEM EXECUTAR o programa.
+// cosonle log (a,b,c) = undefined, undefined e 10
+
+//Analise o programa abaixo, entenda o que ele faz e sugira melhores nomes para as variáveis. Lembre-se que devemos escolher 
+//nomes significativos, usar o padrão camelCase. Alem disso, os nomes não podem começar com números ou caracteres especiais.
+
+// let horasTrabalhadas = prompt ("Quantas horas você trabalha por dia?")
+// let recebePorDia = prompt ("Quanto você recebe por dia?")
+// alert(`Voce recebe ${t/p} por hora`)
+
+
+//  let nome 
+//  let idade 
+
+//  console.log(comoSeChama,valorIdade)
+
+// não atribui valores ao nome e idade por isso ficaram como undefined
+
+// let nome = prompt ("Qual seu Nome?")
+// let idade = prompt ("Qual Sua idade?")
+
+// pois os nome e idade já foi declarado, mesmo que esteja undefined.
+
+const meuNome = ("Matheus")
+const minhaIdade = 26
+
+console.log("Olá Meu nome é", meuNome, "E minha idade é ", minhaIdade);
+
+
+let choveu = ("sim")
+let temperatura = 35
+let horas = ('14 horas')
+
+console.log("Hoje Choveu?", choveu,
+    ("Qual temperatura de hoje?"), temperatura,
+    ("Que horas são?"), horas)
+
+    let a = 10
+    let b = 25
+    let c = 10
+    a = c
+    a = b
+    b = c
+    
+    
+
+    console.log("O novo valor de a é ",a)
+    console.log("O novo valor de b é ",b)
+    
