80e9640e4e742cd97f135cfbf40a4e7f616a729e
Matheus Augusto Moura de Lima <61365431+Mouramattheus@users.noreply.github.com>
2022-04-08 14:26:11 -0300

    atualizacao-lista-labefy

--- modulo-2/projeto-labefy/projeto-labefy/src/components/TelaPlaylist.js
+++ modulo-2/projeto-labefy/projeto-labefy/src/components/TelaPlaylist.js
@@ -1,74 +1,85 @@
-import React from "react"
-import axios from "axios"
-import styled from "styled-components"
+import React from "react";
+import axios from "axios";
+import styled from "styled-components";
 
 const CardPlaylist = styled.div`
-    border: 1px solid black;
-    padding: 10px;
-    margin: 10px;
-    width: 300px;
-    display: flex;
-    justify-content: space-between;
-`
-
+  border: 1px solid black;
+  padding: 10px;
+  margin: 10px;
+  width: 300px;
+  display: flex;
+  justify-content: space-between;
+`;
 
 export default class TelaListalistaPlaylist extends React.Component {
-   
-    state = {
-        
-        listaPlaylist: []
-        
-    }
-    
+  state = {
+    listaPlaylists: [],
+  };
+//   componentDidMount() {
+//     this.getAllPlaylists();
+//   }
 
-    getAllPlaylists = () => {
-        const url = "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists"
-        axios.get(url, {
-            headers: {
-                Authorization: "chijo-paiva"
-            }
-        }).then((res) => {
-            this.setState({ listaPlaylist: res.data })
-        })
-        .catch((err) => {
-            alert("Ocorreu um problema, tente novamente")
-        })
+  getAllPlaylists = () => {
+    const url =
+      "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists";
+    axios
+      .get(url, {
+        headers: {
+          Authorization: "matheus-moura-shaw",
+        },
+      })
+      .then((response) => {
+        this.setState({ listaPlaylists: response.data.result.list });
+      })
+      .catch((err) => {
+        alert("Ocorreu um problema, tente novamente");
+      });
+    //listaPlaylists =  meu state
+  };
 
-       
-    }
+  componentDidMount() {
+    this.getAllPlaylists();
+  }
 
-    deletarUsuario = (id) => {
-        const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/:playlistId${id}`
-        axios.delete(url, {
-            headers: {
-                Authorization: "matheus-moura-shaw"
-            }
-        })
-            .then((res) => {
-                alert("Playlist deletada com sucesso!")
-                this.getAllPlaylists()
-            })
-            .catch((err) => {
-                alert("Ocorreu um erro, tente novamente")
-            })
-    }
+  deletarUsuario = (id) => {
+    const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}`;
+    axios
+      .delete(url, {
+        headers: {
+          Authorization: "matheus-moura-shaw",
+        },
+      })
+      .then((res) => {
+        alert("Deseja Deletar essa Playlist?");
+        this.getAllPlaylists();
+      })
+      .catch((err) => {
+        alert("Ocorreu um erro, tente novamente");
+      });
+  };
 
-    render() {
-        const playListas = this.state.listaPlaylist.map((lista) => {
-            return (
-                <CardPlaylist key={lista.id}>
-                    {lista}
-                    <button onClick={() => this.deletarUsuario(lista.id)}>X</button>
-                </CardPlaylist>
-            )
-        })
+  render() {
+    console.log(this.state.listaPlaylists);
 
-        return (
-            <div>
-                <button onClick={this.props.criarPlaylist}>Ir Para Criar Playlist</button>
-                <h2>Lista de Usuários</h2>
-                {playListas}
-            </div>
-        )
-    }
+    const playListas = this.state.listaPlaylists.map((lista) => {
+      return (
+      <div>
+          
+          {lista.name}
+          <button onClick={() => this.deletarUsuario(lista.id)}>X</button>
+        </div>
+      );
+    });
+    // console.log(playListas)
+
+    return (
+      <div>
+        <button onClick={this.props.criarPlaylist}>
+          Ir Para Criar Playlist
+        </button>
+        <h2>Lista de Usuários</h2>
+        {playListas}
+      </div>
+    );
+  }
 }
