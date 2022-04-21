c3675572b55d90a67a415443740c27870eb79a0d
Matheus Augusto Moura de Lima <61365431+Mouramattheus@users.noreply.github.com>
2022-04-21 17:26:36 -0300

    astromatch-card-atualizado

--- modulo-3/astromatch/app/src/App.js
+++ modulo-3/astromatch/app/src/App.js
@@ -3,104 +3,97 @@ import styled from 'styled-components'
 import axios from 'axios'
 
 
-
 const url = `https://us-central1-missao-newton.cloudfunctions.net/astroMatch/matheus-moura-shaw/`
 
 
+
+//estilização
 const Main = styled.div`
-background-color: blue;
-border: 2px solid black;
-justify-content: center;
-width: 50%;
-align-items: center;
- 
+/* padding: 40px; */
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 `
 
 const FotoPerfil = styled.img`
+/* display: flex; */
+`
 
-
-  margin-right: 10px;
-  border-radius: 50%;
- 
+const CardPerfil = styled.div`
+width: 30%;
+  height: 30rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  background-color: lightcyan;
+  border-color: black;
+  align-items: center;
+  img {
+    width: 40%;
+  }
 
 `
 
+const BotaoLike = styled.div`
+display: flex;
 
 
 
+
+
+`
 const  App = ()=> {
   const [profile, setProfile] = useState({})
-
   const getProfiletoChoose = () =>{
     axios
     .get(`${url}person`)
     .then((res) =>{
       setProfile(res.data.profile)
-
-    }) 
+    })
     .catch((res)=>{
       console.log(res.response)
-
     })
-
   }
-
-
   const choosePerson =(id, boolean) =>{
     const body = {
-      id: id, 
+      id: id,
       boolean: boolean
       }
-    
     axios
-    .get(`${url}choose-person`,body)
+    .post(`${url}choose-person`,body)
     .then(()=>{
       getProfiletoChoose()
-
     })
     .catch((res)=>{
       console.log(res.response)
     })
   }
-
   useEffect(()=>{
     getProfiletoChoose()
-  
   },[])
-
-
-
   // const getMatches = () =>{
   //   axios
   //   .get(`${url}choose-person`)
   //   .then((res)=>{
-
   //   })
   //   .catch((err)=>{
-
   //   })
   // }
-
-
-
-
-
-
-
-
-
-
-
   return (
     <Main>
-      <FotoPerfil src={profile.photo}/>
+      <CardPerfil>
+      <FotoPerfil src={profile.photo} />
       <p>{profile.name} {profile.age}</p>
-
-
+      <p>{profile.bio}</p>
+     </CardPerfil>
+      <BotaoLike>
+      <button>Deslike</button> <button>Like</button>
+      </BotaoLike>
+      
+      
 </Main>
-
-    
   );
 }
-
 export default App;
---
+++ modulo-3/astromatch/app/src/Styled.js
@@ -0,0 +1,28 @@
+import styled ,{keyframes} from "styled-components";
+
+
+
+//animação do cartão 
+export const swipeRight = keyframes`
+  from {
+    opacity: 1;
+	  transform: translate(0) rotate(0);
+  }
+
+  to {
+    opacity: 0;
+	  transform: translate(-200px) rotate(-20deg);
+  }
+`;
+export const swipeLeft = keyframes`
+  from {
+    opacity: 1;
+	  transform: translate(0) rotate(0);
+  }
+
+  to {
+    opacity: 0;
+	  transform: translate(200px) rotate(20deg);
+  }
+`;
+
