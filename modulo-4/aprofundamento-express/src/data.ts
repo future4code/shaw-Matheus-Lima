69ab2c6fc1658c82afc8f2eeb35892ef55f10c52
Matheus Augusto Moura de Lima <61365431+Mouramattheus@users.noreply.github.com>
2022-05-31 15:59:14 -0300

    aprofundament-express

---
+++ modulo-4/aprofundamento-express/src/data.ts
@@ -0,0 +1,34 @@
+
+
+export type afazer ={
+  userId:number,
+  id:number,
+  title:string,
+  completed:boolean
+}
+
+
+
+
+export const afazeres:afazer[] = [{
+  userId:1,
+  id:1,
+  title:"lavar a louça",
+  completed:false
+
+},
+
+{
+  userId:2,
+  id:2,
+  title:"passar a roupa",
+  completed:true
+},
+{
+  userId:3,
+  id:3,
+  title:"criar um jutsu",
+  completed:false,
+}
+
+]
---
+++ modulo-4/aprofundamento-express/src/index.ts
@@ -0,0 +1,80 @@
+import express, { response } from "express"
+import cors from "cors"
+import { afazeres } from "./data"
+import { afazer } from "./data"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(3003, () => console.log("Servidor disponível em 3003"))
+
+app.get("/ping",(req,res)=>{
+  res.send("pong🏓")
+})
+
+app.get("/afazeres",(req,res)=>{
+  const tarefas = afazeres.filter((afazer)=>{
+    if (afazer.completed === true){
+      return afazer
+    }
+  })
+  res.send(tarefas)
+  
+})
+
+app.post("/criar-tarefa/",(req,res)=>{
+
+ const {userId,id,title,completed}:afazer = req.body
+
+if (!userId || !id || !title || completed){
+  response.status(404).send("Dados userId, id, tittle ou completed faltando!, Preencha novamente!") 
+}
+const criarNovaTarefa = {
+  userId:userId,
+  id:id,
+  title:title,
+  completed:completed,
+
+}
+afazeres.push(criarNovaTarefa)
+res.status(201).send(criarNovaTarefa)
+
+
+
+})
+
+
+app.put ("/afazeres/troca/:idTarefa",(req,res)=>{
+    afazeres.find((tarefa)=>{
+    if(Number(req.params.idTarefa) === tarefa.id){
+      return tarefa.completed = req.body.completed
+    }
+  })
+  res.send(afazeres)
+  console.log("Status da Tarefa alterada!")
+})
+
+
+app.delete ("/afazeres/:idTarefa",(req,res)=>{
+
+   afazeres.filter((tarefa)=>{
+    if(Number(req.params.idTarefa) === tarefa.id) {
+      return afazeres.splice(tarefa.id -1, 1)
+    }
+    res.send(afazeres)
+    console.log("Tarefa Deletada!")
+  })
+})
+
+
+app.get ("/afazeres/user/:idUser",(req,res)=>{
+  const idFiltrado = afazeres.filter((tarefa)=>{
+    if(Number(req.params.idUser)===(tarefa.userId)){
+      return tarefa
+    }
+    res.send(idFiltrado)
+    console.log("Tarefas filtradas")
+  })
+})
