925e5779a2cbbb9cfc9bb4631d282add873ccf69
Matheus Augusto Moura de Lima <61365431+Mouramattheus@users.noreply.github.com>
2022-06-03 17:21:31 -0300

    sistema-bancario

---
+++ modulo-4/sistema-bancario/src/data.ts
@@ -0,0 +1,43 @@
+import { Account } from "./index";
+
+
+export const ListaBanco: Account[] = [
+    {   id:"cliente1",
+        name: "João",
+        cpf: "223.112.225.12",
+        birth: new Date("1958-05-22"),
+        saldoDebit:13.000,
+        extrato: {
+            valorPag: 200,
+            dayPay: "22/12/2022",
+            descrPag: "Pagar a conta de luz"
+        }
+
+
+    },
+    {   id:"cliente1",
+        name: "Jorge",
+        cpf: "613.903.400-07",
+        birth: new Date("1958-05-22"),
+        saldoDebit:600,
+        extrato: {
+            valorPag: 500,
+            dayPay: "25/09/2022",
+            descrPag: "Pagar a conta de água"
+        }
+    },
+    {   id:"cliente1",
+        name: "Luana",
+        cpf: "057.209.190-74",
+        birth: new Date("1958-05-22"),
+        saldoDebit:500,
+        extrato: {
+            valorPag: 13.000,
+            dayPay: "19/11/2022",
+            descrPag: "pagar a passagem para europa"
+        }
+    }
+
+]
+
+
---
+++ modulo-4/sistema-bancario/src/index.ts
@@ -0,0 +1,155 @@
+import express from "express"
+import cors from "cors"
+import { ListaBanco } from "./data"
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.listen(3003, () => {
+    console.log("server 3003 is running!")
+})
+
+app.get("/hello", (req, res) => {
+    res.send("world")
+})
+// ----- Criar conta 
+// Criação de conta só é possivel para o cliente se tiver idade => 18 anos informar "NOME", "CPF" "DATA DE NASCIMENTO"
+// conta deve guardar essa informação e uma propriedade para o saldo
+// todas as contas começam com saldo = 0 
+// array de extrato possuindo valor e data e uma descrição 
+
+// ----- Pegar Saldo
+//pegar saldo usuário deve conseguir verificar a conta apenas se passar seu "NOME" E "CPF"
+
+
+// ---- Adicionar saldo
+// O usuário só consegue adicionar saldo a sua conta passando "NOME" , "CPF" e o "VALOR QUE DESEJA ADD"
+
+// ---- Pagar conta 
+// usuário consegue pagar uma conta passando um "VALOR" uma "DESCRIÇÃO DO PAGAMENTO" e "DATA DE PAGAMENTO",
+// caso não informe a "DATA" deve ser considerado que o pagamento é para ser feito no mesmo dia.
+// usuário NÃO pode agendar um PAGAMENTO para dia que já passou ou tentar PAGAR uma CONTA  cujo valor seja maior que seu saldo
+
+
+//---- Transferencia interna
+// Para realizar a transferência USUÁRIO deve informar seu "NOME", "CPF". "NOME DESTINATÁRIO", o "CPF DO DESTINATÁRIO" e o "VALOR"
+// transferências não podem ser agendadas e devem respeitar os saldos da conta do remetente.
+
+
+export type Account = {
+    id: string
+    name: string,
+    cpf: string,
+    birth: Date
+    saldoDebit: number
+    extrato: Saldo
+
+}
+
+
+export type Saldo = {
+    valorPag: number,
+    descrPag: string,
+    dayPay: string
+}
+
+
+// retorna todos os usuários do banco 
+app.get("/users", (req, res) => {
+    res.send(ListaBanco)
+})
+
+
+
+// cria usuários do banco
+app.post("/users", (req, res) => {
+    try {
+        const id = Math.floor(Date.now() * Math.random()).toString(36)
+
+        const { name, cpf, birth, saldoDebit, extrato } = req.body
+
+        const newDate = new Date(birth)
+
+        const idade = Math.floor((Date.now() - newDate.getTime()) / (365.25 * 24 * 60 * 60 * 1000))
+
+        if (idade < 18) {
+            throw new Error("Somente maiores de 18 anos podem criar a conta no banco!")
+
+        }
+
+        // cria novo cliente
+        const createClient: Account = {
+            id: id,
+            name: name,
+            cpf: cpf,
+            birth: birth,
+            saldoDebit: saldoDebit,
+            extrato: extrato
+        }
+        if(!name || !cpf || !birth || !saldoDebit){
+            throw new Error("Campos não foram preenchidos corretamente por favor preencha novamente!")
+        }
+
+
+        ListaBanco.push(createClient)
+        res.status(200).send(ListaBanco)
+        console.log("Usuário Criado com Sucesso!")
+
+
+    } catch (error: any) {
+        switch (error.message) {
+            case "Somente maiores de 18 anos podem criar a conta no banco!":
+                res.status(400)
+                break;
+                case "Campos não foram preenchidos corretamente por favor preencha novamente!":
+                    res.status(400)
+                    break
+
+            default:
+                break;
+        }
+        res.send(error.message)
+
+    }
+})
+
+
+app.get("/saldo/:cpf/:name",(req,res)=>{
+    try {
+          const {cpf, name} = req.params
+    
+          if(!cpf || !name){
+        throw new Error("Dados em Falta por favor escreva novamente!")
+    }
+
+    const filtroSaldo = ListaBanco.filter((user)=>{
+        if (cpf === user.cpf && name === user.name)
+        return user.saldoDebit
+    })
+
+    const saldoFiltrado = filtroSaldo.map((saldo)=>{
+        if(saldo !== undefined){
+            return saldo.saldoDebit
+        }
+    })
+    res.status(200).send(saldoFiltrado)
+        
+    } catch (error:any) {
+        switch (error.message) {
+
+            case "Dados em Falta por favor escreva novamente!":
+                res.status(400)
+                break;
+        
+            default:
+                break;
+        }
+        res.send(error.message)
+
+    }
+    
+  
+    
+})
+
